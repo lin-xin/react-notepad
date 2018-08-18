73df428f6aa3873fad6cf6c63dd2f1bfba98a6a1
lin-xin <2981207131@qq.com>
2018-08-18 18:00:38 +0800

    '列表'

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,48 +1,75 @@
 import React, { Component } from 'react';
 import './style.less';
 
+const ListType = (props) => {
+    const ListContent = () => {
+        return props.data.filter((item, index) => {
+            console.log(item);
+            if(item.type===1){
+                return (
+                    <li className="event-list" key={item.id}>
+                        <input type="checkbox" />
+                        <div>{item.content}</div>
+                        <button className="cancel-btn">取消</button>
+                    </li>
+                )
+            }else if(item.type===2){
+                return (
+                    <li className="event-list" key={item.id}>
+                        <input type="checkbox"/>
+                        <div></div>
+                        <span className="event-time"></span>
+                    </li>
+                )
+            }else{
+                return (
+                    <li className="event-list" key={item.id}>
+                        <div className="event-delete"></div>
+                        <button className="cancel-btn">恢复</button>
+                    </li>
+                )
+            }
+        })
+    }
+    return (
+        <div>
+            <div className="event-tab">{props.type===1?'未完成':(props.type===2?'已完成':'已取消')}
+                <span></span>
+            </div>
+            <div className="event-box">
+                <ul>
+                    <ListContent/>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Data = [
+    {
+        id: 1,
+        type: 1,
+        content: '哈哈哈哈'
+    },
+    {
+        id: 2,
+        type: 2,
+        content: 'sdasd'
+    },
+    {
+        id: 3,
+        type: 3,
+        content: '哈哈哈fdgfgd哈'
+    }
+]
+
 class AppList extends Component{
     render(){
         return (
             <div className="event-content">
-                <div className="event-tab">未完成
-                    <span></span>
-                </div>
-                <div className="event-box">
-                    <ul>
-                        <li className="event-list" v-for="value in getToDo">
-                            <input type="checkbox" />
-                            <div></div>
-                            <button className="cancel-btn">取消</button>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="event-tab">已完成
-                    <span></span>
-                </div>
-                <div className="event-box">
-                    <ul>
-                        <li className="event-list" v-for="value in getDone">
-                            <input type="checkbox"/>
-                            <div></div>
-                            <span className="event-time"></span>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="event-tab">已取消
-                    <span></span>
-                </div>
-                <div className="event-box">
-                    <ul>
-                        <li className="event-list" v-for="value in getCancel">
-                            <div className="event-delete"></div>
-                            <button className="cancel-btn">恢复</button>
-                        </li>
-                    </ul>
-                </div>
-
+                <ListType type={1} data={Data}/>
+                {/* <ListType type={2} data={Data}/>
+                <ListType type={3} data={Data}/> */}
             </div>
         )
     }
