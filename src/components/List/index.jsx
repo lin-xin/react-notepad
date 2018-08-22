3491e21a28294f3ec2c52399724ed99a86b1e4f4
lin-xin <2981207131@qq.com>
2018-08-22 11:18:15 +0800

    '完成事件状态改变'

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,77 +1,98 @@
 import React, { Component } from 'react';
+import getDate from '../../utils/date';
 import './style.less';
 
-const ListType = (props) => {
-    const ListContent = () => {
-        return props.data.filter((item, index) => {
-            console.log(item);
-            if(item.type===1){
-                return (
-                    <li className="event-list" key={item.id}>
-                        <input type="checkbox" />
-                        <div>{item.content}</div>
-                        <button className="cancel-btn">取消</button>
-                    </li>
-                )
-            }else if(item.type===2){
-                return (
-                    <li className="event-list" key={item.id}>
-                        <input type="checkbox"/>
-                        <div></div>
-                        <span className="event-time"></span>
-                    </li>
-                )
-            }else{
-                return (
-                    <li className="event-list" key={item.id}>
-                        <div className="event-delete"></div>
-                        <button className="cancel-btn">恢复</button>
-                    </li>
-                )
-            }
-        })
-    }
-    return (
-        <div>
-            <div className="event-tab">{props.type===1?'未完成':(props.type===2?'已完成':'已取消')}
-                <span></span>
-            </div>
-            <div className="event-box">
-                <ul>
-                    <ListContent/>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-const Data = [
-    {
-        id: 1,
-        type: 1,
-        content: '哈哈哈哈'
-    },
-    {
-        id: 2,
-        type: 2,
-        content: 'sdasd'
-    },
-    {
-        id: 3,
-        type: 3,
-        content: '哈哈哈fdgfgd哈'
-    }
-]
 
 class AppList extends Component{
+    state = {
+        data : [
+            {
+                id: 1,
+                type: 1,
+                content: '哈哈哈哈'
+            },
+            {
+                id: 2,
+                type: 1,
+                content: 'sdasd'
+            },
+            {
+                id: 3,
+                type: 3,
+                content: '哈哈哈fdgfgd哈'
+            },
+            {
+                id: 4,
+                type: 1,
+                content: '哈哈上谁水水水水是'
+            }
+        ]
+    }
     render(){
+        const ListContent = (props) => {
+            return props.data.map(item => {
+                if(props.type === 1 && item.type === 1){
+                    return (
+                        <li className="event-list" key={item.id}>
+                            <input type="checkbox" onClick={id => {this.handleChangeType(item.id, 2)}}/>
+                            <div>{item.content}</div>
+                            <button className="cancel-btn" onClick={id => {this.handleChangeType(item.id, 3)}}>取消</button>
+                        </li>
+                    )
+                }else if(props.type === 2 && item.type === 2){
+                    return (
+                        <li className="event-list" key={item.id}>
+                            <input type="checkbox" defaultChecked onClick={id => {this.handleChangeType(item.id, 1)}}/>
+                            <div>{item.content}</div>
+                            <span className="event-time">{item.time}</span>
+                        </li>
+                    )
+                }else if(props.type === 3 &&  item.type === 3){
+                    return (
+                        <li className="event-list" key={item.id}>
+                            <div className="event-delete">{item.content}</div>
+                            <button className="cancel-btn" onClick={id => {this.handleChangeType(item.id, 1)}}>恢复</button>
+                        </li>
+                    )
+                }
+            })
+        }
+        
+        const ListType = (props) => {
+            return (
+                <div>
+                    <div className="event-tab">{props.type===1?'未完成':(props.type===2?'已完成':'已取消')}
+                        <span></span>
+                    </div>
+                    <div className="event-box">
+                        <ul>
+                            <ListContent data={props.data} type={props.type}/>
+                        </ul>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="event-content">
-                <ListType type={1} data={Data}/>
-                {/* <ListType type={2} data={Data}/>
-                <ListType type={3} data={Data}/> */}
+                <ListType type={1} data={this.state.data}/>
+                <ListType type={2} data={this.state.data}/>
+                <ListType type={3} data={this.state.data}/>
             </div>
         )
+    }
+    handleChangeType = (id, type) => {
+        const data = this.state.data;
+        for (let i = 0, len = data.length; i < len; i++) {
+            if(data[i].id === id){
+                data[i].type = type;
+            }
+            if(type === 2){
+                data[i].time = getDate(new Date());
+            }
+        }
+        this.setState({
+            data
+        })
     }
 }
 
