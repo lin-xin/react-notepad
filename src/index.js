9012846854fe0acc7c9ad1089d2d5a59c4f4e909
lin-xin <2981207131@qq.com>
2019-04-24 15:29:28 +0800

    更新react版本为最新16.8.6

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import store from './store/store';
 import './index.less';
 import './theme.less';
 import App from './components/Home/index';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('app'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
